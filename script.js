c465a8196ddc527b2081c4316778798a068e1fb5
Mr. Sanjoy <--add>
2024-05-27 21:32:35 +0530

    basic captcha id working

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+const captchaDiv = document.getElementById('captcha-div');
+
+console.log(captchaDiv);
+
+function generateCaptcha() {
+    let uniqueCaptcha = '';
+    const captchaChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 1; i < 6; i++) {
+        uniqueCaptcha += captchaChar.charAt(
+            Math.random() * captchaChar.length)
+    }
+    captchaDiv.innerHTML = uniqueCaptcha
+} 
+const refreshImg = document.getElementById('refresh-img');
+refreshImg.addEventListener('click', generateCaptcha);
