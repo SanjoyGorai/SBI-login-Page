d186ed00bf2072c52738f7eade8f919bb272f460
Mr. Sanjoy <--add>
2024-05-28 12:10:02 +0530

    basics captcha shoe hide is working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const captchaDiv = document.getElementById('captcha-div');
-
+// captchaDiv.style.visibility = 'hidden';
 console.log(captchaDiv);
 
 function generateCaptcha() {
@@ -11,6 +11,40 @@ function generateCaptcha() {
             Math.random() * captchaChar.length)
     }
     captchaDiv.innerHTML = uniqueCaptcha
-} 
+}
 const refreshImg = document.getElementById('refresh-img');
 refreshImg.addEventListener('click', generateCaptcha);
+
+
+const userName = window.document.getElementById('username-field');
+userName.addEventListener('paste', e => alert('Paste is not allwoed'));
+
+
+const imageRadio = document.getElementById('image-radio');
+const audioRadio = document.getElementById('audio-radio');
+const captchaAudio = document.getElementById('captcha-audio');
+
+audioRadio.addEventListener('change', e => showHide())
+imageRadio.addEventListener('change', e => showHide())
+
+
+
+function showHide() {
+    if (audioRadio.checked) {
+        captchaAudio.hidden = false;
+        refreshImg.style.visibility = 'hidden';
+        captchaDiv.style.visibility = 'hidden'
+    } else {
+        refreshImg.style.visibility = 'visible'
+        captchaAudio.hidden = true;
+        // captchaAudio.style.visibility = 'hidden'
+        captchaDiv.style.visibility = 'visible'
+    }
+
+    if (imageRadio.checked) {
+        console.log('ffff');
+      
+    } 
+}
+
+
